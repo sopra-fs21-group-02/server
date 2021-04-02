ec0d371633027376cf8953bd9d16b4161bd6db1c
nehasingh1491 <nehasingh1491@gmail.com>
2021-04-02 15:14:18 +0200

    added google api

--- src/main/java/ch/uzh/ifi/hase/soprafs21/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/controller/UserController.java
@@ -1,15 +1,29 @@
 package ch.uzh.ifi.hase.soprafs21.controller;
 
 import ch.uzh.ifi.hase.soprafs21.entity.User;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.LoginPostDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs21.service.UserService;
+import com.fasterxml.jackson.core.JsonFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Payload;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.GsonFactoryBean;
+import org.springframework.web.bind.annotation.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 
 /**
  * User Controller
@@ -53,5 +67,52 @@ public class UserController {
 
         // convert internal representation of user back to API
         return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Boolean getUserDetails(@RequestBody LoginPostDTO loginPostDTO) throws GeneralSecurityException, IOException {
+
+        System.out.println("tokenid:::::"+loginPostDTO.getTokenId());
+        JsonFactory factory = new GsonFactory();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), factory)
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+        GoogleIdToken idToken = verifier.verify(loginPostDTO.getTokenId());
+
+        if (idToken != null) {
+
+            Payload payload = idToken.getPayload();
+            if(loginPostDTO.getEmailId()==payload.getEmail()) {
+
+                // Print user identifier
+                String userId = payload.getSubject();
+                System.out.println("User ID: " + userId);
+
+                // Get profile information from payload
+                String email = payload.getEmail();
+                boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+                String locale = (String) payload.get("locale");
+                String familyName = (String) payload.get("family_name");
+                String givenName = (String) payload.get("given_name");
+
+                // Use or store profile information
+                // ...
+                return true;
+            }
+            return false;
+        } else {
+            System.out.println("Invalid ID token.");
+            return false;
+        }
+
     }
 }
