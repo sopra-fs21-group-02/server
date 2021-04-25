700cee218a6330c6265d7ca5c9a7419edabc4b18
nehasingh1491 <nehasingh1491@gmail.com>
2021-04-25 22:35:07 +0200

    Added Test Cases for Authentication Filter

--- src/test/java/ch/uzh/ifi/hase/soprafs21/security/JwtTokenAuthenticationFilterTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/security/JwtTokenAuthenticationFilterTest.java
@@ -1,12 +1,14 @@
 package ch.uzh.ifi.hase.soprafs21.security;
 
+import ch.uzh.ifi.hase.soprafs21.entity.User;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.OnlineStatusDto;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserOverviewDto;
+import ch.uzh.ifi.hase.soprafs21.security.config.SecurityConstants;
 import ch.uzh.ifi.hase.soprafs21.service.JwtTokenUtil;
 import ch.uzh.ifi.hase.soprafs21.service.UserService;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -18,15 +20,14 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
+import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.*;
 
 class JwtTokenAuthenticationFilterTest {
 
-    @Mock
+
     JwtTokenAuthenticationFilter jwtFilter;
 
     @Mock
@@ -57,14 +58,70 @@ class JwtTokenAuthenticationFilterTest {
     }
 
     @Test
-    void doFilterInternal() throws ServletException, IOException {
+    void testDoFilterInternal() throws ServletException, IOException {
         HttpServletRequest  mockedRequest = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse mockedResponse = Mockito.mock(HttpServletResponse.class);
         FilterChain mockedFilterChain = Mockito.mock(FilterChain.class);
-        jwtFilter.doFilterInternal(mockedRequest,mockedResponse,mockedFilterChain);
+        Claims mockedClaims = Mockito.mock(Claims.class);
 
-        doNothing().when(jwtFilter).doFilterInternal(isA(HttpServletRequest.class), isA(HttpServletResponse.class), isA(FilterChain.class));
-        jwtFilter.doFilterInternal(mockedRequest, mockedResponse, mockedFilterChain);
-        verify(jwtFilter, times(1)).doFilterInternal(mockedRequest, mockedResponse, mockedFilterChain);
+        User mockedUser = User.builder().id(1l).email("email1").name("name1").profilePictureURL("url1").build();
+
+        when(mockedRequest.getHeader(SecurityConstants.HEADER_STRING)).thenReturn("Bearer Test123");
+        when(jwtTokenUtil.validateToken("Test123",SecurityConstants.SECRET)).thenReturn(Boolean.TRUE);
+        when(jwtTokenUtil.getClaimsFromJWT("Test123",SecurityConstants.SECRET)).thenReturn(mockedClaims);
+        when(mockedClaims.getSubject()).thenReturn("email1");
+        when(userServiceMock.getUserByEmail("email1")).thenReturn(Optional.of((User) mockedUser));
+
+        jwtFilter.doFilterInternal(mockedRequest,mockedResponse,mockedFilterChain);
+        verify(jwtTokenUtil,times(1)).validateToken("Test123",SecurityConstants.SECRET);
+        verify(jwtTokenUtil,times(1)).getClaimsFromJWT("Test123",SecurityConstants.SECRET);
+        verify(userServiceMock,times(1)).getUserByEmail("email1");
+
+    }
+
+    @Test
+    void testDoFilterInternalNoHeader() throws ServletException, IOException {
+        HttpServletRequest  mockedRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse mockedResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain mockedFilterChain = Mockito.mock(FilterChain.class);
+
+        when(mockedRequest.getHeader(SecurityConstants.HEADER_STRING)).thenReturn(null);
+
+        jwtFilter.doFilterInternal(mockedRequest,mockedResponse,mockedFilterChain);
+        verify(jwtTokenUtil,times(0)).validateToken("Test123",SecurityConstants.SECRET);
+        verify(jwtTokenUtil,times(0)).getClaimsFromJWT("Test123",SecurityConstants.SECRET);
+        verify(userServiceMock,times(0)).getUserByEmail("email1");
+
+    }
+
+    @Test
+    void testDoFilterInternalWrongHeader() throws ServletException, IOException {
+        HttpServletRequest  mockedRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse mockedResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain mockedFilterChain = Mockito.mock(FilterChain.class);
+
+        when(mockedRequest.getHeader(SecurityConstants.HEADER_STRING)).thenReturn("Null");
+
+        jwtFilter.doFilterInternal(mockedRequest,mockedResponse,mockedFilterChain);
+        verify(jwtTokenUtil,times(0)).validateToken("Test123",SecurityConstants.SECRET);
+        verify(jwtTokenUtil,times(0)).getClaimsFromJWT("Test123",SecurityConstants.SECRET);
+        verify(userServiceMock,times(0)).getUserByEmail("email1");
+
+    }
+
+    @Test
+    void testDoFilterInternalInvalidToken() throws ServletException, IOException {
+        HttpServletRequest  mockedRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse mockedResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain mockedFilterChain = Mockito.mock(FilterChain.class);
+
+        when(mockedRequest.getHeader(SecurityConstants.HEADER_STRING)).thenReturn("Bearer Test123");
+        when(jwtTokenUtil.validateToken("Test123",SecurityConstants.SECRET)).thenReturn(Boolean.FALSE);
+
+        jwtFilter.doFilterInternal(mockedRequest,mockedResponse,mockedFilterChain);
+        verify(jwtTokenUtil,times(1)).validateToken("Test123",SecurityConstants.SECRET);
+        verify(jwtTokenUtil,times(0)).getClaimsFromJWT("Test123",SecurityConstants.SECRET);
+        verify(userServiceMock,times(0)).getUserByEmail("email1");
+
     }
 }
