3259e15b72e5db0fbbd3687552be2e16a327c50e
Ksenia <beloturkinakl@gmail.com>
2021-05-02 15:05:23 +0200

    bugfixes

--- src/main/java/ch/uzh/ifi/hase/soprafs21/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/UserRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
