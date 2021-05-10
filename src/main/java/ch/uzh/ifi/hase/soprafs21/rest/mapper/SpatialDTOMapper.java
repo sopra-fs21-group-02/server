b275f5679dbd88542ca53fc78528261ed233b824
nehasingh1491 <nehasingh1491@gmail.com>
2021-05-10 22:03:02 +0200

    merged changes from master and requested changes from PR

--- src/main/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/SpatialDTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/SpatialDTOMapper.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.hase.soprafs21.rest.mapper;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.CoordinateDto;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.Point;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -30,5 +31,9 @@ public interface SpatialDTOMapper {
 
     default Point getPoint(CoordinateDto coordinateDto, GeometryFactory geometryFactory) {
         return geometryFactory.createPoint(new Coordinate(coordinateDto.getLongitude(), coordinateDto.getLatitude()));
+    }
+
+    default LineString getLineString(List<CoordinateDto> coordinateDtoList, GeometryFactory geometryFactory) {
+        return geometryFactory.createLineString(getCoordinates(coordinateDtoList));
     }
 }
