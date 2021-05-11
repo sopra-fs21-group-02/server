e93ad21f49cfb658d34585bab63d6c39b79aa2c0
Ksenia <beloturkinakl@gmail.com>
2021-05-11 23:03:13 +0200

    Get parks in area endpoint, ParkApiController test, ParkService integration test, Rest interface test

--- src/test/java/ch/uzh/ifi/hase/soprafs21/controller/ParksApiControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/controller/ParksApiControllerTest.java
@@ -2,10 +2,8 @@ package ch.uzh.ifi.hase.soprafs21.controller;
 
 import ch.uzh.ifi.hase.soprafs21.entity.Park;
 import ch.uzh.ifi.hase.soprafs21.entity.User;
-import ch.uzh.ifi.hase.soprafs21.rest.dto.ChatMessagePostDto;
-import ch.uzh.ifi.hase.soprafs21.rest.dto.CoordinateDto;
-import ch.uzh.ifi.hase.soprafs21.rest.dto.ParkDto;
-import ch.uzh.ifi.hase.soprafs21.service.ChatService;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.*;
+import ch.uzh.ifi.hase.soprafs21.rest.mapper.SpatialDTOMapper;
 import ch.uzh.ifi.hase.soprafs21.service.ParkService;
 import ch.uzh.ifi.hase.soprafs21.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,18 +11,20 @@ import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -49,6 +49,15 @@ class ParksApiControllerTest {
 
     @InjectMocks
     private ParksApiController parksApiController;
+
+    @Mock
+    private Polygon polygon;
+
+    @Mock
+    private Park park1Mock;
+
+    @Mock
+    private Park park2Mock;
 
 
     @BeforeEach
@@ -82,5 +91,31 @@ class ParksApiControllerTest {
         String expectedMessage = "Id is not allowed in POST";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void getParksByArea() throws Exception {
+        CoordinateDto coordinateDto1 = new CoordinateDto();
+        coordinateDto1.setLongitude(4.5555);
+        coordinateDto1.setLatitude(47.5555);
+        CoordinateDto coordinateDto2 = new CoordinateDto();
+        coordinateDto2.setLongitude(5.5585);
+        coordinateDto2.setLatitude(25.5545);
+        CoordinateDto coordinateDto3 = new CoordinateDto();
+        coordinateDto3.setLongitude(8.5585);
+        coordinateDto3.setLatitude(22.5545);
+        AreaFilterDto areaFilter = new AreaFilterDto();
+        areaFilter.addVisibleAreaItem(coordinateDto1).addVisibleAreaItem(coordinateDto2).addVisibleAreaItem(coordinateDto3);
+        Coordinate [] coordinates = SpatialDTOMapper.INSTANCE.getCoordinates(areaFilter.getVisibleArea());
+        when(geometryFactoryMock.createPolygon(eq (coordinates))).thenReturn(polygon);
+
+        when(park1Mock.getCreator()).thenReturn(creator);
+        when(park2Mock.getCreator()).thenReturn(creator);
+        List<Park> listParks = Arrays.asList(park1Mock, park2Mock);
+        when(parkServiceMock.getAllParksInArea(any())).thenReturn(listParks);
+
+        ResponseEntity<List<ParkDto>> entity = parksApiController.getParks(areaFilter);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals(2, entity.getBody().size());
     }
 }
